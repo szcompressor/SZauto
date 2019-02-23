1512f2a3b9f21ba2ff08ff2384d0c52bef939dc6
Xin Liang <xin@Xins-MacBook-Pro-2.local>
2019-02-23 09:52:32 -0500

    update compile

--- test_compile.sh
+++ test_compile.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-SZ_dir=/Users/xin/utils/sz_master/
-g++ -O3 -c sz_compression_utils.cpp -o a.o
-g++ -O3 -c sz_decompression_utils.cpp -o b.o
-g++ -O3 -c sz_compress_3d.cpp -o c.o #-I$SZ_dir/include
-g++ -O3 -c sz_decompress_3d.cpp -o d.o #-I$SZ_dir/include
-g++ -O3 sz_compress_test.cpp a.o b.o c.o d.o #-I$SZ_dir/include -L$SZ_dir/lib -lSZ -lzlib -lzstd -lm
+SZ_dir=/Users/xin/utils/sz/
+g++ -O3 -c sz_compression_utils.cpp -o a.o -I$SZ_dir/include
+g++ -O3 -c sz_decompression_utils.cpp -o b.o -I$SZ_dir/include
+g++ -O3 -c sz_compress_3d.cpp -o c.o -I$SZ_dir/include
+g++ -O3 -c sz_decompress_3d.cpp -o d.o -I$SZ_dir/include
+g++ -O3 sz_compress_test.cpp a.o b.o c.o d.o -I$SZ_dir/include -L$SZ_dir/lib -lSZ -lzlib -lzstd -lm
