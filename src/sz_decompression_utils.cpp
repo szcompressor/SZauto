81a1d5fcb4f5fb37a559c40aa3435336baffe174
Xin Liang <xin@eduroam062-161.wl.anl-external.org>
2019-04-24 13:44:07 -0500

    missed

--- src/sz_decompression_utils.cpp
+++ src/sz_decompression_utils.cpp
@@ -1,34 +1,5 @@
 #include "sz_decompression_utils.hpp"
 
-float *
-decode_regression_coefficients(const unsigned char *& compressed_pos, size_t reg_count, int block_size, float precision){
-	size_t reg_unpredictable_count = 0;
-	read_variable_from_src(compressed_pos, reg_unpredictable_count);
-	const float * reg_unpredictable_data_pos = (const float *) compressed_pos;
-	compressed_pos += reg_unpredictable_count * sizeof(float);
-	int * reg_type = Huffman_decode_tree_and_data(2*RegCoeffCapacity, RegCoeffNum3d*reg_count, compressed_pos);
-	float * reg_params = (float *) malloc(RegCoeffNum3d*(reg_count + 1)*sizeof(float));
-	for(int i=0; i<RegCoeffNum3d; i++)
-		reg_params[i] = 0;
-	float reg_precisions[RegCoeffNum3d];
-	float rel_param_err = RegErrThreshold * precision / RegCoeffNum3d;
-	for(int i=0; i<RegCoeffNum3d-1; i++)
-		reg_precisions[i] = rel_param_err / block_size;
-	reg_precisions[RegCoeffNum3d - 1] = rel_param_err;
-	float * prev_reg_params = reg_params;
-	float * reg_params_pos = reg_params + RegCoeffNum3d;
-	const int * type_pos = (const int *) reg_type;
-	for(int i=0; i<reg_count; i++){
-		for(int j=0; j<RegCoeffNum3d; j++){
-			*reg_params_pos = recover(*prev_reg_params, reg_precisions[j], *(type_pos++), RegCoeffRadius, reg_unpredictable_data_pos);
-			prev_reg_params ++, reg_params_pos ++;
-		}
-	}
-	free(reg_type);
-	return reg_params;
-}
-
-
 // modified from TypeManager.c
 // change return value and increment compressed_pos
 unsigned char * 
