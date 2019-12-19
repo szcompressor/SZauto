8b40417e7314e18a6d58f914b2419cbb9c896918
Kai Zhao <kazhao@beboplogin1.lcrc.anl.gov>
2019-12-19 17:39:11 -0600

    add sh

---
+++ experiment/nyx.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=sz-qmc
+#SBATCH -p bdwall
+#SBATCH -A ECP-EZ
+#SBATCH --nodes 1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+
+# srun -N 1 -n 1 /home/kazhao/meta_compressor/build/bin/sz_test1 /home/kazhao/data/288x115x69x69.f32.qmc 33120 69 69 1e-2 >qmc1e-2 2>&1 &
+# srun -N 1 -n 1 /home/kazhao/meta_compressor/build/bin/sz_test1 /home/kazhao/data/288x115x69x69.f32.qmc 33120 69 69 1e-3 >qmc1e-3 2>&1 &
+# srun -N 1 -n 1 /home/kazhao/meta_compressor/build/bin/sz_test1 /home/kazhao/data/288x115x69x69.f32.qmc 33120 69 69 1e-4 >qmc1e-4 2>&1 &
+# srun -N 1 -n 1 /home/kazhao/meta_compressor/build/bin/sz_test1 /home/kazhao/data/288x115x69x69.f32.qmc 33120 69 69 1e-5 >qmc1e-5 2>&1 &
+# srun -N 1 -n 1 /home/kazhao/meta_compressor/build/bin/sz_test1 /home/kazhao/data/288x115x69x69.f32.qmc 33120 69 69 1e-6 >qmc1e-6 2>&1 &
+# srun -N 1 -n 1 /home/kazhao/meta_compressor/build/bin/sz_test1 /home/kazhao/data/288x115x69x69.f32.qmc 33120 69 69 1e-7 >qmc1e-7 2>&1 &
+# srun -N 1 -n 1 /home/kazhao/meta_compressor/build/bin/sz_test1 /home/kazhao/data/288x115x69x69.f32.qmc 33120 69 69 1e-8 >qmc1e-8 2>&1 &
+#wait
+
+
+(~/meta_compressor/build/bin/sz_test1 ~/data/6x512x512x512.f32.nyx 3072 512 512 1e-2 >nyx1e-2 2>&1 &)
+(~/meta_compressor/build/bin/sz_test1 ~/data/6x512x512x512.f32.nyx 3072 512 512 1e-3 >nyx1e-3 2>&1 &)
+(~/meta_compressor/build/bin/sz_test1 ~/data/6x512x512x512.f32.nyx 3072 512 512 1e-4 >nyx1e-4 2>&1 &)
+(~/meta_compressor/build/bin/sz_test1 ~/data/6x512x512x512.f32.nyx 3072 512 512 1e-5 >nyx1e-5 2>&1 &)
+(~/meta_compressor/build/bin/sz_test1 ~/data/6x512x512x512.f32.nyx 3072 512 512 1e-6 >nyx1e-6 2>&1 &)
+(~/meta_compressor/build/bin/sz_test1 ~/data/6x512x512x512.f32.nyx 3072 512 512 1e-7 >nyx1e-7 2>&1 &)
+(~/meta_compressor/build/bin/sz_test1 ~/data/6x512x512x512.f32.nyx 3072 512 512 1e-8 >nyx1e-8 2>&1 &)
+
+
