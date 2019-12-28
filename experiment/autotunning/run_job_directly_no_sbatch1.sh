2eb97e3dd92d21fc3e54693f3e02fbc941741c3c
kzhao <zhaokaijia@gmail.com>
2019-12-27 21:59:13 -0600

    show more compress info

--- experiment/autotunning/run_job_directly_no_sbatch1.sh
+++ experiment/autotunning/run_job_directly_no_sbatch1.sh
@@ -9,6 +9,14 @@
 
 chmod +x /home/kazhao/meta_compressor/experiment/autotunning/single_job.sh
 
+/home/kazhao/meta_compressor/experiment/autotunning/single_job.sh qmcpack-288x115x69x69 einspline_288_115_69_69.pre.f32.dat 33120 69 69
+/home/kazhao/meta_compressor/experiment/autotunning/single_job.sh miranda-256x384x384 density.f32.dat  256 384 384
+/home/kazhao/meta_compressor/experiment/autotunning/single_job.sh miranda-256x384x384 diffusivity.f32.dat 256 384 384
+/home/kazhao/meta_compressor/experiment/autotunning/single_job.sh miranda-256x384x384 pressure.f32.dat  256 384 384
+/home/kazhao/meta_compressor/experiment/autotunning/single_job.sh miranda-256x384x384 velocityx.f32.dat  256 384 384
+/home/kazhao/meta_compressor/experiment/autotunning/single_job.sh miranda-256x384x384 velocityy.f32.dat  256 384 384
+/home/kazhao/meta_compressor/experiment/autotunning/single_job.sh miranda-256x384x384 velocityz.f32.dat  256 384 384
+/home/kazhao/meta_compressor/experiment/autotunning/single_job.sh miranda-256x384x384 viscocity.f32.dat  256 384 384
 /home/kazhao/meta_compressor/experiment/autotunning/single_job.sh hurricane-100x500x500 CLOUDf48.bin.dat 100 500 500
 /home/kazhao/meta_compressor/experiment/autotunning/single_job.sh hurricane-100x500x500 PRECIPf48.bin.dat 100 500 500
 /home/kazhao/meta_compressor/experiment/autotunning/single_job.sh hurricane-100x500x500 Pf48.bin.dat 100 500 500
