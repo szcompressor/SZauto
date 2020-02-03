d47b601fb35358e457e7939990dda6bc29b8d91e
kzhao <zhaokaijia@gmail.com>
2020-02-03 15:20:27 -0500

    autotuning v5. add sampling method test

--- experiment/selector/run_selctor.sh
+++ experiment/selector/run_selctor.sh
@@ -18,8 +18,8 @@ for eb in 1E-2 5E-3 1E-3 5E-4 1E-4 5E-5 1E-5 1E-6 1E-7
 do
   mkdir -p $run/$dataset/$eb
   python $res/extract_and_run.py $exe $field $r1 $r2 $r3 $eb $run/$dataset/$eb
-  #echo ~/meta_compressor/build/bin/sz_autotuning ~/data/$dataset/$field $r1 $r2 $r3 $ebs
-  #~/meta_compressor/build/bin/sz_autotuning ~/data/$dataset/$field $r1 $r2 $r3 $eb > $run/$dataset/$eb/$field.log  2>&1 &
+  #echo ~/code/meta_compressor/build/bin/sz_autotuning ~/data/$dataset/$field $r1 $r2 $r3 $ebs
+  #~/code/meta_compressor/build/bin/sz_autotuning ~/data/$dataset/$field $r1 $r2 $r3 $eb > $run/$dataset/$eb/$field.log  2>&1 &
 done
 done
 wait
