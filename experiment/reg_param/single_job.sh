148451f16736389d68f040edb621b9216c208a2a
kzhao <zhaokaijia@gmail.com>
2020-02-18 09:45:06 -0600

    regression param lossy vs lossless test

--- experiment/reg_param/single_job.sh
+++ experiment/reg_param/single_job.sh
@@ -8,9 +8,10 @@ r3=$5
 
 for eb in 1E-2 5E-3 1E-3 5E-4 1E-4 5E-5 1E-5 1E-6 1E-7 1E-8 1E-9 5E-9 1E-10
 do
-  mkdir -p $run/$dataset/$eb
+  mkdir -p $run/reg1/$dataset/$eb
+  mkdir -p $run/reg2/$dataset/$eb
   echo ~/code/meta_compressor/build/bin/sz_test1 ~/data/$dataset/$field $r1 $r2 $r3 $ebs
-  ~/code/meta_compressor/build/bin/sz_test1 ~/data/$dataset/$field $r1 $r2 $r3 $eb 6 3 1 1 65536 > $run/$dataset/$eb/$field.log  2>&1 &
-  ~/code/meta_compressor/build/bin/sz_test1 ~/data/$dataset/$field $r1 $r2 $r3 $eb 6 3 1 2 65536 > $run/$dataset/$eb/$field.log  2>&1 &
+  ~/code/meta_compressor/build/bin/sz_test1 ~/data/$dataset/$field $r1 $r2 $r3 $eb 6 3 0 1 65536 > $run/reg1/$dataset/$eb/$field.log  2>&1 &
+  ~/code/meta_compressor/build/bin/sz_test1 ~/data/$dataset/$field $r1 $r2 $r3 $eb 6 3 0 2 65536 > $run/reg2/$dataset/$eb/$field.log  2>&1 &
 done
 wait
