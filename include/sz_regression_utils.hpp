698c12e5c7b2fa6b52f127ec27b8d9720dade1f4
Kai Zhao <zhaokaijia@gmail.com>
2021-07-27 11:24:21 -0400

    move poly regression coeff to hpp

--- include/sz_regression_utils.hpp
+++ include/sz_regression_utils.hpp
@@ -1,10 +1,13 @@
 #ifndef _sz_regression_utils_hpp
 #define _sz_regression_utils_hpp
+
 #include "sz_utils.hpp"
+#include "sz_poly_regression_coeff_aux.hpp"
 #include "sz_def.hpp"
 #include <array>
 #include <iomanip>
 #include <algorithm>
+
 #define COEF_AUX_MAX_BLOCK 17
 
 inline size_t get_coef_aux_list_idx(const std::array<size_t, 3> &dims) {
@@ -18,6 +21,7 @@ inline size_t get_coef_aux_list_idx(const std::array<size_t, 3> &dims) {
     }
     return coef_aux_index;
 }
+
 template<typename T>
 void display_coef_aux(std::array<T, RegPolyCoeffNum3d * RegPolyCoeffNum3d> aux) {
     for (int i = 0; i < RegPolyCoeffNum3d; i++) {
@@ -30,14 +34,8 @@ void display_coef_aux(std::array<T, RegPolyCoeffNum3d * RegPolyCoeffNum3d> aux) 
 //template<typename T>
 std::vector<std::array<float, RegPolyCoeffNum3d * RegPolyCoeffNum3d>> init_poly() {
 
-    size_t num = 0;
-    float* data;
-#ifdef POLY_REGRESSION_COEF_FILE
-    data = readfile<float>(POLY_REGRESSION_COEF_FILE, num);
-#else
-    printf("cannot find poly regression aux file.");
-    exit(1);
-#endif
+    float *data = COEFF_3D;
+    size_t num = sizeof(COEFF_3D) / sizeof(float);;
 
     auto coef_aux_p = &data[0];
     std::vector<std::array<float, RegPolyCoeffNum3d * RegPolyCoeffNum3d>> coef_aux_list = std::vector<std::array<float,
@@ -48,7 +46,8 @@ std::vector<std::array<float, RegPolyCoeffNum3d * RegPolyCoeffNum3d>> init_poly(
         for (auto &idx:dims) {
             idx = *coef_aux_p++;
         }
-        std::copy_n(coef_aux_p, RegPolyCoeffNum3d * RegPolyCoeffNum3d, coef_aux_list[get_coef_aux_list_idx(dims)].begin());
+        std::copy_n(coef_aux_p, RegPolyCoeffNum3d * RegPolyCoeffNum3d,
+                    coef_aux_list[get_coef_aux_list_idx(dims)].begin());
         coef_aux_p += RegPolyCoeffNum3d * RegPolyCoeffNum3d;
     }
 //    display_coef_aux(coef_aux_list[get_coef_aux_list_idx( std::array<size_t, 3>{6, 6, 6})]);
@@ -56,50 +55,53 @@ std::vector<std::array<float, RegPolyCoeffNum3d * RegPolyCoeffNum3d>> init_poly(
     return coef_aux_list;
 }
 
-template <typename T>
+template<typename T>
 inline void
-compute_regression_coeffcients_3d(const T * data_pos, int size_x, int size_y, int size_z, size_t dim0_offset, size_t dim1_offset, float * reg_params_pos){
-	/*Calculate regression coefficients*/
-	const T * cur_data_pos = data_pos;
-	float fx = 0.0;
-	float fy = 0.0;
-	float fz = 0.0;
-	float f = 0;
-	float sum_x, sum_y;
-	T curData;
-	for(int i=0; i<size_x; i++){
-		sum_x = 0;
-		for(int j=0; j<size_y; j++){
-			sum_y = 0;
-			for(int k=0; k<size_z; k++){
-				curData = *cur_data_pos;
-				sum_y += curData;
-				fz += curData * k;
-				cur_data_pos ++;
-			}
-			fy += sum_y * j;
-			sum_x += sum_y;
-			cur_data_pos += (dim1_offset - size_z);
-		}
-		fx += sum_x * i;
-		f += sum_x;
-		cur_data_pos += (dim0_offset - size_y * dim1_offset);
-	}
-	float coeff = 1.0 / (size_x * size_y * size_z);
-	reg_params_pos[0] = (2 * fx / (size_x - 1) - f) * 6 * coeff / (size_x + 1);
-	reg_params_pos[1] = (2 * fy / (size_y - 1) - f) * 6 * coeff / (size_y + 1);
-	reg_params_pos[2] = (2 * fz / (size_z - 1) - f) * 6 * coeff / (size_z + 1);
-	reg_params_pos[3] = f * coeff - ((size_x - 1) * reg_params_pos[0] / 2 + (size_y - 1) * reg_params_pos[1] / 2 + (size_z - 1) * reg_params_pos[2] / 2);
+compute_regression_coeffcients_3d(const T *data_pos, int size_x, int size_y, int size_z, size_t dim0_offset,
+                                  size_t dim1_offset, float *reg_params_pos) {
+    /*Calculate regression coefficients*/
+    const T *cur_data_pos = data_pos;
+    float fx = 0.0;
+    float fy = 0.0;
+    float fz = 0.0;
+    float f = 0;
+    float sum_x, sum_y;
+    T curData;
+    for (int i = 0; i < size_x; i++) {
+        sum_x = 0;
+        for (int j = 0; j < size_y; j++) {
+            sum_y = 0;
+            for (int k = 0; k < size_z; k++) {
+                curData = *cur_data_pos;
+                sum_y += curData;
+                fz += curData * k;
+                cur_data_pos++;
+            }
+            fy += sum_y * j;
+            sum_x += sum_y;
+            cur_data_pos += (dim1_offset - size_z);
+        }
+        fx += sum_x * i;
+        f += sum_x;
+        cur_data_pos += (dim0_offset - size_y * dim1_offset);
+    }
+    float coeff = 1.0 / (size_x * size_y * size_z);
+    reg_params_pos[0] = (2 * fx / (size_x - 1) - f) * 6 * coeff / (size_x + 1);
+    reg_params_pos[1] = (2 * fy / (size_y - 1) - f) * 6 * coeff / (size_y + 1);
+    reg_params_pos[2] = (2 * fz / (size_z - 1) - f) * 6 * coeff / (size_z + 1);
+    reg_params_pos[3] = f * coeff - ((size_x - 1) * reg_params_pos[0] / 2 + (size_y - 1) * reg_params_pos[1] / 2 +
+                                     (size_z - 1) * reg_params_pos[2] / 2);
 }
 
-template <typename T>
+template<typename T>
 inline void
-compute_regression_coeffcients_3d_poly(const T * data_pos, int size_x, int size_y, int size_z, size_t dim0_offset, size_t dim1_offset,
-        float * reg_params_pos, std::vector<std::array<float, 100>> &coef_aux_list){
+compute_regression_coeffcients_3d_poly(const T *data_pos, int size_x, int size_y, int size_z, size_t dim0_offset,
+                                       size_t dim1_offset,
+                                       float *reg_params_pos, std::vector<std::array<float, 100>> &coef_aux_list) {
 
     std::array<size_t, 3> dims = {(size_t) size_x, (size_t) size_y, (size_t) size_z};
     std::array<double, RegPolyCoeffNum3d> sum{0};
-    const T * cur_data_pos = data_pos;
+    const T *cur_data_pos = data_pos;
     T curData;
     for (int i = 0; i < size_x; i++) {
         for (int j = 0; j < size_y; j++) {
